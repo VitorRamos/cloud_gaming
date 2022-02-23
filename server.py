223d5765c8cd8a0af2632bc73b8af976d851e293
vitorramos <ramos.vitor89@gmail.com>
2022-02-23 09:21:28 +0100

    rename files

--- server.py
+++ server.py
@@ -1,73 +1,42 @@
 import socket
 import threading
-import keyboard as kbd
-import time
-from Xlib import X, display, XK
-from Xlib.ext import record
-from Xlib.protocol import rq
+import win32api, win32con
+import vkcodes
+import json
 
+addr = ("localhost", 2020)
 
-key_mapping = {}
-for name in dir(XK):
-    if name[:3] == "XK_":
-        key_mapping[getattr(XK, name)] = name[3:]
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_sock.bind(addr)
+server_sock.listen(5)
 
 def keyboard(cnn):
-    record_dpy = display.Display()
-    local_dpy = display.Display()
-    
-    def keycallback(event):
-        data = event.data
-        while len(data):
-            event, data = rq.EventField(None).parse_binary_value(data, record_dpy.display, None, None)
+    while 1:
+        data = cnn.recv(1024)
+        aux_ = data.split(b"\r")[:-1]
+        while len(aux_):
+            aux = aux_.pop()
+            aux = aux.decode().split(" ")
+            key = aux[0]
+            state = aux[1]
+            try:
+                vk = vkcodes.VK_CODE[key]
+                keyup = win32con.KEYEVENTF_KEYUP if state == "1" else 0
+                win32api.keybd_event(vk, 0, keyup, 0)
+            except:
+                pass
+            print(data)
 
-            if event.type == X.KeyPress:
-                keysym = local_dpy.keycode_to_keysym(event.detail, 0)
-                try:
-                        symb = key_mapping[keysym].lower()
-                except:
-                    return
-                cnn.send(f"{symb} {2}".encode()+b"\r")
-
-            if event.type == X.KeyRelease:
-                keysym = local_dpy.keycode_to_keysym(event.detail, 0)
-                try:
-                        symb = key_mapping[keysym].lower()
-                except:
-                    return
-                cnn.send(f"{symb} {1}".encode()+b"\r")
-
-    ctx = record_dpy.record_create_context(
-        0,
-        [record.AllClients],
-        [{
-            'core_requests': (0, 0),
-            'core_replies': (0, 0),
-            'ext_requests': (0, 0, 0, 0),
-            'ext_replies': (0, 0, 0, 0),
-            'delivered_events': (0, 0),
-            'device_events': (X.KeyReleaseMask, X.PointerMotionMask),
-            'errors': (0, 0),
-            'client_started': False,
-            'client_died': False,
-        }])
-        
-    record_dpy.record_enable_context(ctx, keycallback)
-    record_dpy.record_free_context(ctx)
 
 def mouse(cnn):
     while 1:
-        cnn.send(b"mouse")
-        time.sleep(1)
+        data = cnn.recv(1024)
+        print(data)
 
-
-addr = ("localhost", 2020)
-kbd_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-kbd_sock.connect(addr)
-kbd_sock.send(b"keyboard")
-threading.Thread(target=keyboard, args=(kbd_sock,)).start()
-
-mouse_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mouse_sock.connect(addr)
-mouse_sock.send(b"mouse")
-threading.Thread(target=mouse, args=(mouse_sock,)).start()
+while 1:
+    cnn, addr = server_sock.accept()
+    data = cnn.recv(1024)
+    if data == b"keyboard":
+        threading.Thread(target=keyboard, args=(cnn,)).start()
+    if data == b"mouse":
+        threading.Thread(target=mouse, args=(cnn,)).start()
