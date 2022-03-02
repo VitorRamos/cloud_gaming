0fd5204efbeaa0df41a0085f7c6db0d07b0ca949
vitorramos <ramos.vitor89@gmail.com>
2022-03-01 21:00:20 -0300

    display server

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 import threading
-import win32api, win32con
+import win32api, win32con, win32gui, win32ui
 import vkcodes
-import json
+import numpy as np
 
-addr = ("localhost", 2020)
+addr = ("0.0.0.0", 2020)
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_sock.bind(addr)
@@ -28,8 +28,43 @@ def input_events(cnn):
             print(data)
 
 
+def get_screenshot():
+    w = 1024 # set this
+    h = 768 # set this
+    hwnd = None
+    # get the window image data
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+
+    cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
+    # convert the raw data into a format opencv can read
+    signedIntsArray = dataBitMap.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (h, w, 4)
+
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+    # free resources
+    return img
+
+def screen(cnn):
+    while 1:
+        img = get_screenshot()
+        data = img.tobytes()
+        sz = len(data)
+        cnn.send(sz.to_bytes(4,"big")+data)
+        
 while 1:
     cnn, addr = server_sock.accept()
     data = cnn.recv(1024)
+    print(data)
     if data == b"input_events":
         threading.Thread(target=input_events, args=(cnn,)).start()
+    if data == b"screen":
+        threading.Thread(target=screen, args=(cnn,)).start()
